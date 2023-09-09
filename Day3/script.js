6ec5e0d02907ad6a3b0496762b72d96e560ceec3
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-09-09 18:15:34 +0530

    updation

--- Day3/script.js
+++ Day3/script.js
@@ -36,7 +36,7 @@
 
 
  var request = new XMLHttpRequest();
- request.open("GET","https://restcountries.com/v3.1/all");
+ request.open("GET","https://restcountries.com/v3.1/all",true);
  request.send();
  request.onload = function(){
     // var result =  request.response;  the output will be in the string or mixed format
