a0a9fbbfbe8a6292998c7245991a6185e75cfc9f
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-09-10 12:44:35 +0530

    updation of day 5 problem

--- Day5/balancedParanthesis.js
+++ Day5/balancedParanthesis.js
@@ -1,24 +1,55 @@
-let expression = '{({}{(})}';
+let expression = '{({}{(}))}';
 
 function isBalanced(expression) {
-    let array = [];
+    // let array = [];
 
-    for (let bracket of expression) {
-        if (bracket == '{' || bracket == '(') {
-            array.push(bracket);
-        } else if (bracket == '}') {
-            if (array.length == 0) return false;
-            else if (array[array.length - 1] != '{') return false;
-            else array.pop();
-        } else if (bracket == ')') {
-            if (array.length == 0) return false;
-            else if (array[array.length - 1] != '(') return false;
-            else array.pop();
+    // for (let bracket of expression) {
+    //     if (bracket == '{' || bracket == '(') {
+    //         array.push(bracket);
+    //     } else if (bracket == '}') {
+    //         if (array.length == 0) return false;
+    //         else if (array[array.length - 1] != '{') return false;
+    //         else array.pop();
+    //     } else if (bracket == ')') {
+    //         if (array.length == 0) return false;
+    //         else if (array[array.length - 1] != '(') return false;
+    //         else array.pop();
+    //     }
+    // }
+
+    // if (array.length == 0) return true;
+    // else return false;
+
+
+    let openingParanthesisCount = 0;
+    let closingParanthesisCount = 0;
+    let openingBracketCount = 0;
+    let closingBracketCount = 0;
+
+    for(let bracket of expression){
+        if(bracket == '{')
+        {
+            openingParanthesisCount++;
+        }
+        else if(bracket == '(')
+        {
+            openingBracketCount++;
+        }
+        else if(bracket == '}')
+        {
+            closingParanthesisCount++;
+        }
+        else if(bracket == ')')
+        {
+            closingBracketCount++;
         }
     }
-
-    if (array.length == 0) return true;
+    if(openingBracketCount === closingBracketCount && openingParanthesisCount === closingParanthesisCount)
+    {
+        return true;
+    }
     else return false;
+
 }
 
 if (isBalanced(expression)) {
