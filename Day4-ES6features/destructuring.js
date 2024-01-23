58940c226225bc447c79b1b63ddd8ddca5a1b09a
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-01-23 23:51:24 +0530

    Day 4 - ES6 Features

--- Day4-ES6features/destructuring.js
+++ Day4-ES6features/destructuring.js
@@ -9,18 +9,19 @@
 
 
 // destructuring
+// let  [first, second, third] = numbers;
 // let [third, first, second] = numbers;
 
 
-// let [first, second, third] = [1, 2];
-// let [first, second, ...third] = [1, 2, 3, 4, 5];
-
-// let [, , third] = [1, 2, 4, 5, 6];
+// let [first, second, third] = [1, 2];   o/p: 1 2 undefined
+// let [first, second, ...third] = [1, 2, 3, 4, 5]; o/p:  1 2 [3, 4, 5]
 
 
 // console.log(first, second, third);
 
+// let [, , third] = [1, 2, 4, 5, 6];
 // console.log(third);
+
 // let [first, second, third = 0] = [1, 2];
 // let [first, second, third = 0] = [1, 2, 3];
 
--- Day4-ES6features/rest.js
+++ Day4-ES6features/rest.js
@@ -1,6 +1,13 @@
 // rest operator --> it'll be used in arguments of the function
 // ...
 
+// function add(x, y) {
+//     return x+y;
+// }
+// console.log(add(5, 6));
+// console.log(add(5, 6, 7)); rest values will be truncated
+// console.log(add(5, 6, 7, 8, 9));  rest values will be truncated
+
 
 // function add(...args) {
 //     return args;
