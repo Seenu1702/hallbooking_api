05733a69134ef790da52fa106146b7ed49dba01f
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-02-06 02:41:47 +0530

    day 16 updates

--- day16-async/Main.js
+++ day16-async/Main.js
@@ -1,16 +1,31 @@
-const Array = require('./Array');
+// const Array = require('./Array');
 
-let numbers = new Array();
+// let numbers = new Array();
 
-numbers.push(3);
-numbers.push(5);
-numbers.push(7);
-numbers.push(10);
+// numbers.push(3);
+// numbers.push(5);
+// numbers.push(7);
+// numbers.push(10);
 
-let printItem = (value, index, array) => {
-    console.log(value, index, array);
+// let printItem = (value, index, array) => {
+//     console.log(value, index, a;rray);
+// }
+
+// numbers.forEach(printItem);
+
+// // console.log(numbers.toArray());
+// var x = true + false;
+// console.log(x);
+
+// console.log(true === 1)   //false
+// console.log(typeof false)  //boolean
+
+
+// var output = 0;
+// console.log(output ? 'yes' : 'no')
+
+function func(){
+    console.log('hi')
 }
 
-numbers.forEach(printItem);
-
-// console.log(numbers.toArray());
+return func()
